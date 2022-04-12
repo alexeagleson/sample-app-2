f236bd8b589a6ff87b4fc64f2efa6e0713fd4dad
Alex Eagleson <alexeagleson@gmail.com>
2022-04-11 23:49:25 -0400

    build: update component dependency

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import "antd/dist/antd.variable.min.css";
+import type { AppProps } from "next/app";
+import { ConfigProvider } from "antd";
+import { theme } from "tsdx-sample-component-library";
+
+ConfigProvider.config({
+  theme,
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ConfigProvider>
+      <Component {...pageProps} />
+    </ConfigProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
